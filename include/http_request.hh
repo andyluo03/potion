0e3780e03d93d1df3b993eb99e874844f739ed69
Akhil Bharanidhar <akhilb15@outlook.com>
2023-07-25 01:53:01 +0000

    http request interface structure

--- include/http_request.hh
+++ include/http_request.hh
@@ -3,18 +3,49 @@
 
 #include <string>
 #include <map>
+#include <type_traits>
 
 namespace potion {
+
+enum class HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    OPTIONS,
+    CONNECT,
+    TRACE,
+    PATCH
+};
+
+// might vause error bc HTTP_1_0 has same value as GET
+enum class HttpVersion {
+    HTTP_1_0,
+    HTTP_1_1,
+    HTTP_2_0
+};
+
+
 class HttpRequest {
 public:
 
-HttpRequest(std::string);
+HttpRequest(const std::string& raw_request);
 
 private:
 
-std::string method;
-std::string uri;
-std::map<std::string, std::string> headers;
+void ParseRequest(const std::string& raw_request);
+HttpMethod StringToMethod();
+HttpVersion StringToVersion();
+
+std::string uri() const { return uri_; }
+HttpMethod method() const { return method_; }
+
+// members
+HttpMethod method_;
+HttpVersion version_;
+std::string uri_;
+std::map<std::string, std::string> headers_;
 
 };
 }
