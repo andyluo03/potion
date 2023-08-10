a87837316dbf709edfed124e27ac5759238101b4
Akhil Bharanidhar <akhilb15@outlook.com>
2023-08-10 05:57:48 +0000

    clang tidy + clang format

--- include/http_request.hh
+++ include/http_request.hh
@@ -1,11 +1,11 @@
 #ifndef POTION_HTTP_REQ_H
 #define POTION_HTTP_REQ_H
 
-#include "uri.hh"
-
-#include <string>
 #include <map>
+#include <string>
 #include <type_traits>
+
+#include "uri.hh"
 
 namespace potion {
 
@@ -21,37 +21,34 @@ enum class HttpMethod {
     PATCH
 };
 
-
 class HttpRequest {
-public:
+   public:
+    // constructor
+    explicit HttpRequest(const std::string& raw_request);
 
-// constructor
-HttpRequest(const std::string& raw_request);
+    // accessors
+    Uri uri() const { return uri_; }
+    HttpMethod method() const { return method_; }
+    static std::string version() { return "HTTP/1.0"; }
+    std::string body() const { return body_; }
+    std::string header(const std::string& key) const {
+        return headers_.at(key);
+    }
 
-// accessors
-Uri uri() const { return uri_; }
-HttpMethod method() const { return method_; }
-std::string version() const { return "HTTP/1.0"; }
-std::string body() const { return body_; }
-std::string header(const std::string& key) const { return headers_.at(key); }
+   private:
+    void ParseRequest(const std::string& raw_request);
+    void ParseRequestLine(const std::string& req_line);
+    void ParseHeaders(const std::string& headers);
 
-private:
+    HttpMethod StringToMethod(const std::string& method);
 
-void ParseRequest(const std::string& raw_request);
-void ParseRequestLine(const std::string& raw_request);
-void ParseHeaders(const std::string& raw_request);
-
-HttpMethod StringToMethod(const std::string& method);
-
-// members
-HttpMethod method_;
-Uri uri_ = Uri();
-std::map<std::string, std::string> headers_;
-std::string body_;
-
+    // members
+    HttpMethod method_;
+    Uri uri_ = Uri();
+    std::map<std::string, std::string> headers_;
+    std::string body_;
 };
 
-
-} // namespace potion
+}  // namespace potion
 
 #endif
--- include/http_response.hh
+++ include/http_response.hh
@@ -1,49 +1,51 @@
 #ifndef POTION_HTTP_RESP_H
 #define POTION_HTTP_RESP_H
 
-#include <string>
 #include <map>
+#include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace potion {
 
 class HttpResponse {
-public:
-// constructor
-HttpResponse(unsigned int status_code);
+   public:
+    // constructor
+    explicit HttpResponse(unsigned int status_code);
 
-// modifiers
-void set_body(std::string body) { 
-    body_ = body;
-    headers_["Content-Length"] = std::to_string(body.size());
-}
-void set_header(std::string key, std::string value) { headers_[key] = value; }
-// **TODO: add validation for content type
-void set_content_type(std::string content_type) { headers_["Content-Type"] = content_type; }
+    // modifiers
+    void set_body(const std::string& body) {
+        body_ = body;
+        headers_["Content-Length"] = std::to_string(body.size());
+    }
+    void set_header(const std::string& key, std::string value) {
+        headers_[key] = std::move(value);
+    }
+    // **TODO: add validation for content type
+    void set_content_type(std::string content_type) {
+        headers_["Content-Type"] = std::move(content_type);
+    }
 
-// accessors
-std::string body() const { return body_; }
-std::string header(const std::string& key) const { return headers_.at(key); }
-unsigned int status_code() const { return status_code_;}
+    // accessors
+    std::string body() const { return body_; }
+    std::string header(const std::string& key) const {
+        return headers_.at(key);
+    }
+    unsigned int status_code() const { return status_code_; }
 
-// convert to raw response string
-std::string ToString();
+    // convert to raw response string
+    std::string ToString();
 
-private:
+   private:
+    // response data
+    unsigned int status_code_;
+    std::string body_;
+    std::map<std::string, std::string> headers_;
 
-// response data
-unsigned int status_code_;
-std::string body_;
-std::map<std::string, std::string> headers_;
-
-
-// map of status codes to status message strings
-static const std::unordered_map<unsigned int, std::string> status_code_map_;
-
+    // map of status codes to status message strings
+    static const std::unordered_map<unsigned int, std::string> status_code_map_;
 };
 
-
-} // namespace potion
-
+}  // namespace potion
 
 #endif
